f78891e5c10a3011b56fc0837fd06dadece7c675
Steve Davidson <buzzdavidson@me.com>
2011-04-13 19:59:34 -0700

    Renamed DeviceRegistry to DeviceManager

--- src/sphere/plugin/default/coreDevices.py
+++ src/sphere/plugin/default/coreDevices.py
@@ -33,7 +33,7 @@ from sphere.plugin.core.plugin import Plugin
 class CoreDevices(Plugin):
     def __init__(self):
         Plugin.__init__(self, 'CoreDevices')
-        self._description = 'Core Device Plugin'
+        self._description = 'Common elements required for all devices'
 
     def _doStart(self):
         return True
@@ -42,26 +42,41 @@ class CoreDevices(Plugin):
         return True
 
     def _registerDeviceMetadata(self):
-        self._deviceRegistry.registerDeviceCategory('Sensor')
-        self._deviceRegistry.registerDeviceCategory('Actuator')
-        self._deviceRegistry.registerDeviceCategory('Light')
-        self._deviceRegistry.registerDeviceCategory('Appliance')
-        self._deviceRegistry.registerDeviceType(SensorType(name='Thermometer', description='Temperature Sensor', dataType=DeviceType.DATA_TYPES.Float))
-        self._deviceRegistry.registerDeviceType(SensorType(name='Barometer', description='Barometric Pressure Sensor', dataType=DeviceType.DATA_TYPES.Float))
-        self._deviceRegistry.registerDeviceType(SensorType(name='Hygrostat', description='Humidity Sensor', dataType=DeviceType.DATA_TYPES.Float))
-        self._deviceRegistry.registerDeviceType(SensorType(name='Photometer', description='Light Sensor', dataType=DeviceType.DATA_TYPES.Float))
-        self._deviceRegistry.registerDeviceType(SensorType(name='Voltmeter', description='Voltage Sensor', dataType=DeviceType.DATA_TYPES.Float))
-        self._deviceRegistry.registerDeviceType(SensorType(name='Ammeter', description='Current Sensor', dataType=DeviceType.DATA_TYPES.Float))
-        self._deviceRegistry.registerDeviceType(SensorType(name='Counter', description='Counter', dataType=DeviceType.DATA_TYPES.Integer))
-        self._deviceRegistry.registerDeviceType(SensorType(name='Digital', description='General ON/OFF Sensor', dataType=DeviceType.DATA_TYPES.Boolean))
-        self._deviceRegistry.registerDeviceType(SensorType(name='Analog', description='General Analog Sensor', dataType=DeviceType.DATA_TYPES.Float))
+        self._deviceManager.registerDeviceCategory('Sensor')
+        self._deviceManager.registerDeviceCategory('Actuator')
+        self._deviceManager.registerDeviceCategory('Light')
+        self._deviceManager.registerDeviceCategory('Appliance')
+        self._deviceManager.registerDeviceCategory('Interface')
+
+        self._deviceManager.registerDeviceType(SensorType(name='Thermometer', description='Temperature Sensor', dataType=DeviceType.DATA_TYPES.Float))
+        self._deviceManager.registerDeviceType(SensorType(name='Barometer', description='Barometric Pressure Sensor', dataType=DeviceType.DATA_TYPES.Float))
+        self._deviceManager.registerDeviceType(SensorType(name='Hygrostat', description='Humidity Sensor', dataType=DeviceType.DATA_TYPES.Float))
+        self._deviceManager.registerDeviceType(SensorType(name='Photometer', description='Light Sensor', dataType=DeviceType.DATA_TYPES.Float))
+        self._deviceManager.registerDeviceType(SensorType(name='Voltmeter', description='Voltage Sensor', dataType=DeviceType.DATA_TYPES.Float))
+        self._deviceManager.registerDeviceType(SensorType(name='Ammeter', description='Current Sensor', dataType=DeviceType.DATA_TYPES.Float))
+        self._deviceManager.registerDeviceType(SensorType(name='Counter', description='Counter', dataType=DeviceType.DATA_TYPES.Integer))
+        self._deviceManager.registerDeviceType(SensorType(name='Digital', description='General ON/OFF Sensor', dataType=DeviceType.DATA_TYPES.Boolean))
+        self._deviceManager.registerDeviceType(SensorType(name='Analog', description='General Analog Sensor', dataType=DeviceType.DATA_TYPES.Float))
+
         # TODO: pressure sensor
         # TODO: gas sensor, Flow sensor
         # TODO: does it make sense to add subcategories (ie sensor/environmental?)
-        # TODO: security devices: motion detector, breakage detector
         # TODO: weather devices (barometer probably belongs here): rain meter, wind speed, wind direction, UV meter, gas sensor, moisture detector, etc
-        self._deviceRegistry.registerDeviceType(ActuatorType(name='Switch', description='Basic ON/OFF Switch', dataType=DeviceType.DATA_TYPES.Boolean))
-        self._deviceRegistry.registerDeviceType(ActuatorType(name='Relay', description='Remotely Controlled Switch',dataType=DeviceType.DATA_TYPES.Boolean))
+        self._deviceManager.registerDeviceType(ActuatorType(name='Switch', description='Basic ON/OFF Switch', dataType=DeviceType.DATA_TYPES.Boolean))
+        self._deviceManager.registerDeviceType(ActuatorType(name='Relay', description='Remotely Controlled Switch',dataType=DeviceType.DATA_TYPES.Boolean))
         # TODO: define allowed states for dimmer
-        self._deviceRegistry.registerDeviceType(ActuatorType(name='Dimmer', description='Analog Actuator',dataType=DeviceType.DATA_TYPES.Float))
+        self._deviceManager.registerDeviceType(ActuatorType(name='Dimmer', description='Analog Actuator',dataType=DeviceType.DATA_TYPES.Float))
         # TODO: HVAC: heat, cool, fan, damper, humidifier, thermostat
+        self._deviceManager.registerDeviceType(ActuatorType(name='Keypad', description='Keypad',dataType=DeviceType.DATA_TYPES.String))
+
+        self._deviceManager.registerDeviceType(SensorType(name='Motion Detector', description='Motion Detector', dataType=DeviceType.DATA_TYPES.Boolean))
+        self._deviceManager.registerDeviceType(SensorType(name='Breakage', description='Glass Breakage Detector', dataType=DeviceType.DATA_TYPES.Boolean))
+        self._deviceManager.registerDeviceType(SensorType(name='Door', description='Door Open Sensor', dataType=DeviceType.DATA_TYPES.Boolean))
+        self._deviceManager.registerDeviceType(SensorType(name='Window', description='Window Open Sensor', dataType=DeviceType.DATA_TYPES.Boolean))
+        self._deviceManager.registerDeviceType(SensorType(name='Beam', description='IR, Laser, or other beam sensor', dataType=DeviceType.DATA_TYPES.Boolean))
+        self._deviceManager.registerDeviceType(SensorType(name='Proximity', description='Proximity Sensor', dataType=DeviceType.DATA_TYPES.Float))
+        self._deviceManager.registerDeviceType(SensorType(name='RFID', description='RFID Sensor', dataType=DeviceType.DATA_TYPES.String))
+
+        # TODO: power consumption (KWh), water/gas consumption (volume)
+        # TODO: GPS?
+
